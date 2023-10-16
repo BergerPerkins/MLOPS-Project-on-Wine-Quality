e01799db83f42311d3c9f081e338ebf30508a9f4
Berger <your_email_address@example.com>
2023-10-16 21:34:14 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from MLOPS_winequality import logger
+from MLOPS_winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello World")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    dat_ingestion = DataIngestionTrainingPipeline()
+    dat_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
